6ace2e93c2f620afdffa1d54bdb96b012c85c750
villain2 <djvillain@hotmail.com>
2013-11-25 11:04:36 -0500

    adding the test for database connection to localhost

---
+++ php/data.php
@@ -0,0 +1,46 @@
+<?php
+
+function dbConnect()
+{
+    $server         = 'localhost';
+    $database       = 'angularjstest';
+    $username       = 'admin';
+    $password       = 'root';
+    
+    $con            = mysql_connect("$server", "$username", "$password");
+    if(!$con)
+    {
+        die('Could not connect ' . mysql_error());
+    }
+    else
+    {
+        echo 'Connected!!!';
+        
+        $dbcheck    = mysql_select_db("$database");
+        if(!$dbcheck)
+        {
+            echo mysql_error();
+        }
+        else 
+        {
+            echo '<p>You have connected to the database ' . $database . '</p>';
+            
+        }
+    }
+}
+
+function getData()
+{
+    $query          = mysql_query("SELECT * FROM users");
+    
+    $items          = array();
+    while($row = mysql_fetch_array($query))
+    {
+        $items[] = array('fname' => $row['fname'], 'lname' => $row['lname'], 'username' => $row['username']);
+    }
+    echo json_encode(array('items'=>$items));
+}
+
+dbConnect();
+getData();
+?>
